080486c53463f6adaeae66b137c71e13212a3fdd
Leander Lindahl <leanderlindahl@gmail.com>
2019-11-26 22:03:04 +0100

    Add concurrently and nodemon to project Enables console.log to display during development with 'npm dev'

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('this gets logged to console when i compile')
+// Functions (optional and default parameters)
+
+function sum(a: number, b: number = 0): number {
+  return a + b;
+}
+
+sum(1);
+
+type MyFunc = (a: number, b: number) => number;
+const sum2: MyFunc = (a, b) => a + b;
+
+// Unknown number of arguments
+
+function sumEverything(arg1: string, arg2: boolean, ...numbers: number[]): number {
+  return numbers.reduce((result, num) => result + num, 0);
+}
+
+// Overloads
+
+function calcArea(width: number, height: number): number;
+function calcArea(length: number): number;
+function calcArea(...args: number[]): number {
+  if (args.length === 2) {
+    return args[0] * args[1];
+  }
+  return Math.pow(args[0], 2);
+}
+
+console.log('hello');
